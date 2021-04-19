c0186d54ffd1fbbc907b6836f331340487a1db4b
Artem <artem.xolod.90@gmail.com>
2021-04-19 23:01:04 +0300

    создание методов для работы с элементами на странице

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,15 @@
 import './lib/lib';
+import $ from './lib/lib';
 
-$('button').on('click', function(){
-    $(this).toggleClass('active');
+$('button').on('click', function () {
+    $('div').eq(2).toggleClass('active');
+});
+
+$('div').click(function () {
+    console.log($(this).index());
 });
 
 $('.active').removeAttribute('data-modal', 'true').toggleAttribute('data-modal', 'true');
 
-
-console.log('Hello!');
+// console.log($('div').eq(2).finde('.more'));
+console.log($('.mores').eq(0).siblings());
